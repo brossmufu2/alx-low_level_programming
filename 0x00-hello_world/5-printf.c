6dc3ed55e49e82f84737a856d7d5c2b89ab10ec0
brossmufu2 <brossmufu2@gmail.com>
2023-12-15 01:42:58 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
-* main - A program that prints a line with puts functio
+* main - A program that prints a line using the printf  functio
 n
 * Return: 0 (Success)
 */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art, \n")
 return (0);
 }
