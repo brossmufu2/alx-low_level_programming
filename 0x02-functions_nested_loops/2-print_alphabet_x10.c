8f9520788291872b0c940d5448cedf46b8a1f7d3
brossmufu2 <brossmufu2@gmail.com>
2023-12-19 05:09:30 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char p;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (p = 'a'; p <= 'z'; p++)
+	{
+	_putchar(p);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
+
