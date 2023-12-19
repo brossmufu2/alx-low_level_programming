e6f0c2fdd6d0f244cb1c8a49b4e881cb5776090d
brossmufu2 <brossmufu2@gmail.com>
2023-12-19 04:46:29 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char d;
+	for (d = 'a'; d <= 'z'; d++)
+	{
+	_putchar(d);
+	}
+	_putchar('\n');
+}
+
