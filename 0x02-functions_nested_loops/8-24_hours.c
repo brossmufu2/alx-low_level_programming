92a37882f6fff26e857223441b0323f3509fed07
brossmufu2 <brossmufu2@gmail.com>
2023-12-19 08:55:04 -0800

    8-main.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	for (a=0; a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
 	{
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		{
 		_putchar(a + '0');
 		_putchar(b + '0');
-		_putchar(50);
+		_putchar(':');
 		_putchar(c + '0');
 		_putchar(d + '0');
 		_putchar('\n');
