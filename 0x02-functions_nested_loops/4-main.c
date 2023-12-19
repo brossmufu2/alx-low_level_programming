717268d14093ac08b3b16dc21f969e29bad647b1
brossmufu2 <brossmufu2@gmail.com>
2023-12-19 06:14:42 -0800

    5-main.c

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: void
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+
