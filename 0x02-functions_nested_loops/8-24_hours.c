e86b5fbd5e2a444190ef7e6ef5032e31f6984d7e
brossmufu2 <brossmufu2@gmail.com>
2023-12-20 02:54:21 -0800

    fgfg

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,15 @@
 #include "main.h"
+/**
+ * jack_bauer - print 24 hour  moment in life of jack bauer life
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ * @c: The third number to be computed
+ * @d: The fourth number to be computed
+ */
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
