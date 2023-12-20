07672355743035b7c8645ea3f1c7cb97bf9435f0
brossmufu2 <brossmufu2@gmail.com>
2023-12-20 01:47:34 -0800

    dd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- *mail -entry point of my code
+ *main - entry point of my code
  *Return: Always 0
  *
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
