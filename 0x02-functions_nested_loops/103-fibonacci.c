aa1585984d21143c7fcabe0a5391b95c26515279
brossmufu2 <brossmufu2@gmail.com>
2023-12-20 07:25:57 -0800

    mma

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,13 +10,14 @@ int main(void)
 	long j = 1;
         long k = 2;
         long sum = k;
+
 	while (k + j  < 4000000)
 	{
-	k += j;
+		k += j;
 	if (k % 2 == 0)
-	sum += k;
-	j = k - j;
-	++i;
+		sum += k;
+		j = k - j;
+		++i;
 	}
 	printf("%ld\n", sum);
 	return (0);
