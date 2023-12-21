dd98f686408853b1d6a948313054febaf8971eef
brossmufu2 <brossmufu2@gmail.com>
2023-12-21 03:13:00 -0800

    aaa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,13 @@
 * main - A program that print a line using the printF function
 * Retun : 0 (Success)
 */
-int main(void)
+int main()
 {
-printf("with proper grammer , but the outcome is a piece of art, \n");
-return (0);
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+    return 0;
 }
