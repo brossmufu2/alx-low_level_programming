4e1615414747cbc49134a1ef797a304959b08e3c
brossmufu2 <brossmufu2@gmail.com>
2023-12-22 06:39:29 -0800

    SAS

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * nul - Multiplies two integers
- * @x: The first number to multiply
- * @y: The second number to multiply with first
- * number
- * Return: The result of multiplication between x param and y param
- */
-
-int nul(int x, int y)
-{
-	return (x * y);
+int mul(int a, int b) {
+  return a * b;
+return 0;
 }
+
