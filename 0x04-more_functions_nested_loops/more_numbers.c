397b2b9c204d161e53557b181eadaac676a04bdd
brossmufu2 <brossmufu2@gmail.com>
2023-12-24 09:59:16 -0800

    6. The shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+
+                _putchar( j );
+
+        }
+        _putchar('\n');
+    }
+}
